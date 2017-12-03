00f62563db4c918eb6d1a6f712cf0ff78c02de62
Jacob Rafati <yrafati@gmail.com>
2017-12-02 23:06:30 -0800

    decrease minbatch to 64 and m=20

--- LBFGS_LeNet_5.py
+++ LBFGS_LeNet_5.py
@@ -30,7 +30,7 @@ n_classes = data.train.labels.shape[1]  # here MNIST (0-9 digits)
 ########################## HYPER PARAMETER FOR LBFGS ##########################
 ###############################################################################
 # memory limit
-m = 40
+m = 20
 
 #mp = 4
 
@@ -410,7 +410,7 @@ print('----------------------------------------------')
 ################### TO SAVE TRAINING AND TEST LOSS AND ERROR ##################
 ################### FOR REFERENCE NET #########################################
 # Batch size
-minibatch = 1024
+minibatch = 64
 # Total minibatches
 total_minibatches = 400
 # number of minibatches in data
