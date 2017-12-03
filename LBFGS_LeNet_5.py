00aab16be47c7e94edd283b74a6d44841a79130e
Jacob Rafati <yrafati@gmail.com>
2017-12-03 15:47:52 -0800

    m=40

--- LBFGS_LeNet_5.py
+++ LBFGS_LeNet_5.py
@@ -30,7 +30,7 @@ n_classes = data.train.labels.shape[1]  # here MNIST (0-9 digits)
 ########################## HYPER PARAMETER FOR LBFGS ##########################
 ###############################################################################
 # memory limit
-m = 5
+m = 40
 
 # number of weights and bias in each layer
 n_W = {}
