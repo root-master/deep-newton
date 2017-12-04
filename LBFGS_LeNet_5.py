915f43a6423d3a89954ef761c0e495a05e15533a
Jacob Rafati <yrafati@gmail.com>
2017-12-03 18:05:22 -0800

    LB alpha = 0.2

--- LBFGS_LeNet_5.py
+++ LBFGS_LeNet_5.py
@@ -557,7 +557,7 @@ with tf.Session() as sess:
 		############## FINDING ALPHA TO SATISFY ################################
 		############## WOLFE CONDITIONS ########################################
 		########################################################################
-		alpha_step_vec = np.linspace(1.0,0.0,20,dtype='float')
+		alpha_step_vec = np.linspace(1.0,0.2,20,dtype='float')
 		c1 = 1E-4
 		c2 = 0.9
 		old_w = sess.run(weights)
