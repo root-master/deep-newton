5f1f04a52a8ad2eff30ba4685c2cb4ae58b1332f
Jacob Rafati <yrafati@gmail.com>
2017-12-03 17:56:31 -0800

    minibatch = 1024

--- LBFGS_LeNet_5.py
+++ LBFGS_LeNet_5.py
@@ -433,7 +433,7 @@ print('----------------------------------------------')
 ################### TO SAVE TRAINING AND TEST LOSS AND ERROR ##################
 ################### FOR REFERENCE NET #########################################
 # Batch size
-minibatch = 512
+minibatch = 1024
 # Total minibatches
 total_minibatches = 4000
 # number of minibatches in data
