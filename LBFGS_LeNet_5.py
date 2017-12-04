6a29337c4ff9ce6d80e2a2e18da192151834bb07
Jacob Rafati <yrafati@gmail.com>
2017-12-03 16:01:11 -0800

    go back to original mode

--- LBFGS_LeNet_5.py
+++ LBFGS_LeNet_5.py
@@ -556,7 +556,7 @@ with tf.Session() as sess:
 		############## FINDING ALPHA TO SATISFY ################################
 		############## WOLFE CONDITIONS ########################################
 		########################################################################
-		alpha_step_vec = np.linspace(1,0.2,10,dtype='float')
+		alpha_step_vec = np.linspace(1,0.0,20,dtype='float')
 		c1 = 1E-6
 		c2 = 0.9
 		for alpha_step in alpha_step_vec:
