18a0d924d87396160b63c047a2de3af4b969487d
Jacob Rafati <yrafati@gmail.com>
2017-12-04 14:51:54 -0800

    changed params of running

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-declare -a m_vec=(5 10 15 20 40)
-declare -a mini_batch_vec=(64 128 256 512 1024)
+declare -a m_vec=(5 10 15 20)
+declare -a mini_batch_vec=(128 256 512 1024)
 for m in ${m_vec[@]}
 do
 	for minibatch in ${mini_batch_vec[@]}
